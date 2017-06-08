6eb2d1d9b95ffed5f3dade790108bb9281c883df
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-06-09 02:27:21 +0300

    Revert "Feature/date literal token"

--- tests/lexer/test_date_literal_token.py
+++ tests/lexer/test_date_literal_token.py
@@ -6,21 +6,12 @@ from analyzer.syntax_kind import SyntaxKind
 
 
 class TestLexerDateLiteralToken(unittest.TestCase):
-    def test_empty_date(self):
+    # TODO: Поменять название на более адекватное
+    def test_date_(self):
         tokens = list(Lexer(Source("'00010101'")).tokenize())
         self.assertEqual(1, len(tokens))
         self.assertEqual(SyntaxKind.DateLiteralToken, tokens[0][0])
         self.assertEqual("00010101", tokens[0][1])
-
-    def test_empty_date_and_time(self):
-        tokens = list(Lexer(Source("'00010101000000'")).tokenize())
-        self.assertEqual(1, len(tokens))
-        self.assertEqual(SyntaxKind.DateLiteralToken, tokens[0][0])
-        self.assertEqual("00010101000000", tokens[0][1])
-
-    def test_not_date(self):
-        tokens = list(Lexer(Source('''"ДФ=dd.MM.yyyy; ДП='Нет даты'"''')).tokenize())
-        self.assertNotEqual(SyntaxKind.DateLiteralToken, tokens[0][0])
 
 
 if __name__ == '__main__':
