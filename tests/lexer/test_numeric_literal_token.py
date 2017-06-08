45fd63363289af002a3ff1bb8ee25a23c6128b43
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-06-08 13:41:58 +0300

    Рефакторинг класса Lexer

--- tests/lexer/test_numeric_literal_token.py
+++ tests/lexer/test_numeric_literal_token.py
@@ -9,19 +9,19 @@ class TestLexerNumericLiteralToken(unittest.TestCase):
     def test_number_one_digit(self):
         for digit in list("0123456789"):
             with self.subTest(digit):
-                tokens = list(Lexer.tokenize(Source(digit)))
+                tokens = list(Lexer(Source(digit)).tokenize())
                 self.assertEqual(1, len(tokens))
                 self.assertEqual(SyntaxKind.NumericLiteralToken, tokens[0][0])
                 self.assertEqual(digit, tokens[0][1])
 
     def test_number_many_digits(self):
-        tokens = list(Lexer.tokenize(Source("554433")))
+        tokens = list(Lexer(Source("554433")).tokenize())
         self.assertEqual(1, len(tokens))
         self.assertEqual(SyntaxKind.NumericLiteralToken, tokens[0][0])
         self.assertEqual("554433", tokens[0][1])
 
     def test_number_dot_number(self):
-        tokens = list(Lexer.tokenize(Source("3.14")))
+        tokens = list(Lexer(Source("3.14")).tokenize())
         self.assertEqual(1, len(tokens))
         self.assertEqual(SyntaxKind.NumericLiteralToken, tokens[0][0])
         self.assertEqual("3.14", tokens[0][1])
