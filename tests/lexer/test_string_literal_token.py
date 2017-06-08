3c69859c7353c32e9eebafe5dd01669f595a2ed7
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-06-09 00:29:05 +0300

    Тесты: многострочная строка и строка с кавычками внутри

--- tests/lexer/test_string_literal_token.py
+++ tests/lexer/test_string_literal_token.py
@@ -12,6 +12,18 @@ class TestLexerStringLiteralToken(unittest.TestCase):
         self.assertEqual(SyntaxKind.StringLiteralToken, tokens[0][0])
         self.assertEqual("", tokens[0][1])
 
+    def test_string_with_quotation_mark(self):
+        tokens = list(Lexer(Source('"какая-то ""строка"""')).tokenize())
+        self.assertEqual(1, len(tokens))
+        self.assertEqual(SyntaxKind.StringLiteralToken, tokens[0][0])
+        self.assertEqual('какая-то "строка"', tokens[0][1])
+
+    def test_multiline_string(self):
+        tokens = list(Lexer(Source('"многострочная\n|строка"')).tokenize())
+        self.assertEqual(1, len(tokens))
+        self.assertEqual(SyntaxKind.StringLiteralToken, tokens[0][0])
+        self.assertEqual('многострочная\n|строка', tokens[0][1])
+
 
 if __name__ == '__main__':
     unittest.main()
