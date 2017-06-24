9ead00cc787109d9d432eca594cac8e5119edbb5
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-06-25 01:03:47 +0300

    Рефакторинг тестов

--- tests/test_parser/test_expression.py
+++ tests/test_parser/test_expression.py
@@ -1,23 +1,17 @@
 import unittest
-from io import StringIO as Source
 
-from analyzer.lexer import Lexer
-from analyzer.parser import Parser
 from analyzer.syntax_kind import SyntaxKind
+from tests.utils import parse_source
 
 
 class TestParserExpression(unittest.TestCase):
     def test_numeric_literal_expression(self):
-        lexer = Lexer(Source("2")).tokenize()
-        parser = Parser(lexer)
-        node = parser.parse()
-        self.assertEqual(node.kind, SyntaxKind.NumericLiteralExpression)
-        self.assertEqual(node.token.text, "2")
+        syntax_tree = parse_source("2")
+        self.assertEqual(syntax_tree.kind, SyntaxKind.NumericLiteralExpression)
+        self.assertEqual(syntax_tree.token.text, "2")
 
     def test_unary_minus_literal_expression(self):
-        lexer = Lexer(Source("-2")).tokenize()
-        parser = Parser(lexer)
-        node = parser.parse()
-        self.assertEqual(node.kind, SyntaxKind.UnaryMinusExpression)
-        self.assertEqual(node.operand.kind, SyntaxKind.NumericLiteralExpression)
-        self.assertEqual(node.operator_token.kind, SyntaxKind.MinusToken)
+        syntax_tree = parse_source("-2")
+        self.assertEqual(syntax_tree.kind, SyntaxKind.UnaryMinusExpression)
+        self.assertEqual(syntax_tree.operand.kind, SyntaxKind.NumericLiteralExpression)
+        self.assertEqual(syntax_tree.operator_token.kind, SyntaxKind.MinusToken)
