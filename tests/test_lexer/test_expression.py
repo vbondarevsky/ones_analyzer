c5e829b69933f4c3859fadd39d8f8913c7da2e8d
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-07-08 21:05:58 +0300

    Тест trailing_trivia

--- tests/test_lexer/test_expression.py
+++ tests/test_lexer/test_expression.py
@@ -12,3 +12,12 @@ class TestLexerExpression(TestCaseLexer):
         self.check_token(4, SyntaxKind.NumericLiteralToken, [], [])
         self.check_token(5, SyntaxKind.SemicolonToken, [], [])
         self.check_token(6, SyntaxKind.EndOfFileToken, [], [])
+
+    def test_token_with_trailing_trivia(self):
+        self.tokenize_source("А=5; // Комментарий", 5)
+        self.check_token(0, SyntaxKind.IdentifierToken, [], [])
+        self.check_token(1, SyntaxKind.EqualsToken, [], [])
+        self.check_token(2, SyntaxKind.NumericLiteralToken, [], [])
+        self.check_token(3, SyntaxKind.SemicolonToken, [], [SyntaxKind.WhitespaceTrivia,
+                                                            SyntaxKind.SingleLineCommentTrivia])
+        self.check_token(4, SyntaxKind.EndOfFileToken, [], [])
