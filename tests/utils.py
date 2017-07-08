cdc2ef9f1a2744fb770c4b28b70b3c9f9863faaa
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-07-09 00:45:22 +0300

    Проверка на текстовое представление токена

--- tests/utils.py
+++ tests/utils.py
@@ -13,14 +13,15 @@ class TestCaseLexer(TestCase):
     def tokenize_source(self, code, length):
         self.code = code
         self.tokens = list(Lexer(Source(code)).tokenize())
-        self.assertEqual(len(self.tokens), length)
+        self.assertEqual(length, len(self.tokens))
+        self.assertEqual(code, ''.join(map(str, self.tokens)))
 
     def check_token(self, index, kind, leading_trivia, trailing_trivia):
         token = self.tokens[index]
-        self.assertEqual(token.kind, kind)
-        self.assertEqual(len(token.leading_trivia), len(leading_trivia))
-        self.assertEqual(len(token.trailing_trivia), len(trailing_trivia))
+        self.assertEqual(kind, token.kind)
+        self.assertEqual(len(leading_trivia), len(token.leading_trivia))
+        self.assertEqual(len(trailing_trivia), len(token.trailing_trivia))
         for i in range(len(leading_trivia)):
-            self.assertEqual(token.leading_trivia[i].kind, leading_trivia[i])
+            self.assertEqual(leading_trivia[i], token.leading_trivia[i].kind)
         for i in range(len(trailing_trivia)):
-            self.assertEqual(token.trailing_trivia[i].kind, trailing_trivia[i])
+            self.assertEqual(trailing_trivia[i], token.trailing_trivia[i].kind)
